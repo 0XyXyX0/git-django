105753c68041678c5a3922c5f446db4acae9fae2
levani <lburdiladze7@gmail.com>
2024-11-03 15:00:34 +0400

    Delete debug_toolbar_example directory

--- debug_toolbar_example/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-
-from users.models import Product, Person
-
-admin.site.register(Product)
-admin.site.register(Person)
--- debug_toolbar_example/users/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def main(request):
-    context = {"name": "12345"}
-    return render(request, '', context=context)
