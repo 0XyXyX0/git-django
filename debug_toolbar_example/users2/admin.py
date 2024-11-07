d7a56ffa71e71924624debbe08785b8b541c1fd8
levani <lburdiladze7@gmail.com>
2024-11-07 20:12:38 +0400

    Delete debug_toolbar_example directory

--- debug_toolbar_example/users2/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from users2.models import Citizen, Passport, Post, UserProfile
-
-admin.site.register(Citizen)
-admin.site.register(Passport)
-admin.site.register(UserProfile)
-admin.site.register(Post)
